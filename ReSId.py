29ad55d14c1b36d1ac55d57855663d67bed10416
AstroRobin <RobinCook9012@hotmail.com>
2016-03-24 17:10:35 +0800

    Added --datafile option
    datafile takes in the name of the data table of sources to be used by the program

--- ReSId.py
+++ ReSId.py
@@ -20,18 +20,37 @@ import scipy
 import astropy
 
 from optparse import OptionParser
+	
+def read_data(filename):
+	file = open(filename,'r')
+	column_names = file.readline().replace('\n','').split(','); num_columns = len(column_names); #*print column_names
+	lines = file.readlines()
+	
+	in_data = []
+	for ii in range(0,len(lines)):
+		in_data.append(lines[ii].replace('\n','').split(','))
+	
+	print in_data
+		
+def main():
+	usage = "usage: %prog [options] filename.fits"
+	parser = OptionParser()
+	parser.add_option("-f", "--file", dest="filename",
+					  help="write report to FILE", metavar="FILE")
+	parser.add_option("-q", "--quiet",
+					  action="store_false", dest="verbose", default=True,
+					  help="don't print status messages to stdout")
+	parser.add_option("-c", "--centralfreq", 
+					  action="store",type="string",dest="central_freq",
+					  help="provide central frequency", metavar="FREQ")
+	parser.add_option("-d","--datafile",
+					  action="store", dest="data_filename", 
+					  default="C:\\Users\\user\\OneDrive\\Documents\\Uni\\2016 - Semester 1\\Physics Dissertation\\GLEAM\\Data\\IDR3\\GLEAMIDR3.csv",
+					  help="destination of input table for sources",metavar="SOURCESFILE")
+	
+	(options, args) = parser.parse_args()
+	
+	read_data(options.data_filename)
+	
 
-usage = "usage: %prog [options] filename.fits"
-parser = OptionParser()
-parser.add_option("-f", "--file", dest="filename",
-                  help="write report to FILE", metavar="FILE")
-parser.add_option("-q", "--quiet",
-                  action="store_false", dest="verbose", default=True,
-                  help="don't print status messages to stdout")
-parser.add_option("-c", "--freq", 
-				  action="store",type="string",dest="central_freq")
-				  #,help="provide central frequency", metavar="FREQ")
-
-(options, args) = parser.parse_args()
-
-print options.central_freq
+main()

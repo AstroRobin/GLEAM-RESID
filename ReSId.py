530cb228d914de33fd5c67a5d9805d679e033a08
AstroRobin <RobinCook9012@hotmail.com>
2016-03-24 17:42:04 +0800

    Added --ra & --dec options
    --ra and --dec allow the user to input the ra and declination of the area of the sky they wish to use

--- ReSId.py
+++ ReSId.py
@@ -30,11 +30,15 @@ def read_data(filename):
 	for ii in range(0,len(lines)):
 		in_data.append(lines[ii].replace('\n','').split(','))
 	
-	print in_data
-		
+	column_ref = {};
+	for ii in range(0,num_columns):
+		column_ref[column_names[ii]] = ii
+	
+	return (in_data,column_ref)
+	
 def main():
 	usage = "usage: %prog [options] filename.fits"
-	parser = OptionParser()
+	parser = OptionParser(usage=usage)
 	parser.add_option("-f", "--file", dest="filename",
 					  help="write report to FILE", metavar="FILE")
 	parser.add_option("-q", "--quiet",
@@ -43,14 +47,29 @@ def main():
 	parser.add_option("-c", "--centralfreq", 
 					  action="store",type="string",dest="central_freq",
 					  help="provide central frequency", metavar="FREQ")
-	parser.add_option("-d","--datafile",
+	parser.add_option("-i","--datafile",
 					  action="store", dest="data_filename", 
-					  default="C:\\Users\\user\\OneDrive\\Documents\\Uni\\2016 - Semester 1\\Physics Dissertation\\GLEAM\\Data\\IDR3\\GLEAMIDR3.csv",
+					  default="C:\\Users\\user\\OneDrive\\Documents\\Uni\\2016 - Semester 1\\Physics Dissertation\\GLEAM\\Data\\IDR3\\CDFS\\CDFS_sources.csv",
 					  help="destination of input table for sources",metavar="SOURCESFILE")
+	parser.add_option("-r","--ra",
+					  action="store", dest="ra_map",default=None,
+					  help="right ascension of the image",metavar="RA")
+	parser.add_option("-d","--dec",
+					  action="store", dest="dec_map",default=None,
+					  help="declination of the image",metavar="DEC")
+					  
+	#parser.add_option("-","--",
+	#				  action="", dest="",default=,
+	#				  help="")
+	
+	
 	
 	(options, args) = parser.parse_args()
 	
-	read_data(options.data_filename)
+	(in_data, column_ref) = read_data(options.data_filename)
 	
+	#*print in_data
+	print "\n", column_ref["Name"]
+
 
 main()

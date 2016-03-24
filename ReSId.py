b9705645a695a65b6c93b9960729499074223a53
AstroRobin <RobinCook9012@hotmail.com>
2016-03-24 18:04:22 +0800

    RESID edits
    Added --angulardiameter option: 
    takes in the angular diameter of the image to be scanned.
    
    Added extract_sources function: 
    will extract sources from input source file that are constrained within the RA/DEC +ang_diam parameters given.

--- ReSId.py
+++ ReSId.py
@@ -22,6 +22,18 @@ import astropy
 from optparse import OptionParser
 	
 def read_data(filename):
+	"""
+	Read data from file.
+	Take input data of sources and separate into rows that have each been split by column. Also create a dictionary reference of the column names.
+	
+	data format: [[col1,col2,col3,...],[col1,col2,col3,...],[col1,col2,col3,...],...,n_rows]
+	
+	dict format: {0:Names,1:Background_deep,...,34:int_flux_84,...,n_columns}
+	
+	:param filename: name of the input filename
+	:return: The data given from the table in array form, plus a dictionary reference of the column names
+	"""
+
 	file = open(filename,'r')
 	column_names = file.readline().replace('\n','').split(','); num_columns = len(column_names); #*print column_names
 	lines = file.readlines()
@@ -35,6 +47,17 @@ def read_data(filename):
 		column_ref[column_names[ii]] = ii
 	
 	return (in_data,column_ref)
+
+def extract_sources(in_data, RA, DEC, ang_diam):
+	"""
+	Find sources that are positioned with the dimensions of the image specified.
+	
+	:param in_data: data of all sources
+	:param RA: right ascension of the image
+	:param DEC: declination of the image
+	:param ang_diam: angular diameter of the image
+	"""
+		
 	
 def main():
 	usage = "usage: %prog [options] filename.fits"
@@ -46,30 +69,32 @@ def main():
 					  help="don't print status messages to stdout")
 	parser.add_option("-c", "--centralfreq", 
 					  action="store",type="string",dest="central_freq",
-					  help="provide central frequency", metavar="FREQ")
+					  help="provide central frequency", metavar="FREQUENCY")
 	parser.add_option("-i","--datafile",
 					  action="store", dest="data_filename", 
 					  default="C:\\Users\\user\\OneDrive\\Documents\\Uni\\2016 - Semester 1\\Physics Dissertation\\GLEAM\\Data\\IDR3\\CDFS\\CDFS_sources.csv",
-					  help="destination of input table for sources",metavar="SOURCESFILE")
+					  help="destination of input table for sources",metavar="SOURCES_FILE")
 	parser.add_option("-r","--ra",
 					  action="store", dest="ra_map",default=None,
 					  help="right ascension of the image",metavar="RA")
 	parser.add_option("-d","--dec",
 					  action="store", dest="dec_map",default=None,
 					  help="declination of the image",metavar="DEC")
-					  
+	parser.add_option("-a","--angulardiameter",
+					  action="store", type="double", dest="ang_diameter",default=2.0,
+					  help="angular diameter of the sides of the image",metavar="ANGULAR_DIAMETER")
+	
+	
 	#parser.add_option("-","--",
 	#				  action="", dest="",default=,
 	#				  help="")
 	
-	
-	
-	(options, args) = parser.parse_args()
+
+	(options, args) = parser.parse_args()	
 	
 	(in_data, column_ref) = read_data(options.data_filename)
 	
-	#*print in_data
-	print "\n", column_ref["Name"]
+	extract_sources(in_data,options.RA,options.DEC,options.ang_dimaeter)
 
 
 main()

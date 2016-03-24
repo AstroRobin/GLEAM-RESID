92e3464a08fd7c8b38dbae9c3ea15f9f6f2b64ac
AstroRobin <RobinCook9012@hotmail.com>
2016-03-24 15:16:01 +0800

    Include initial option parser framework

---
+++ ReSId.py
@@ -0,0 +1,32 @@
+"""
+The GLEAM REsidual Source IDentifier program
+
+Created by:
+Robin Cook
+March 24 2016
+
+Modifications by:
+Robin Cook
+"""
+
+# Imports
+import sys
+import os
+import numpy as np
+import math
+
+# Other Imports
+import scipy
+import astropy
+
+from optparse import OptionParser
+
+usage = "usage: %prog [options] filename.fits"
+parser = OptionParser()
+parser.add_option("-f", "--file", dest="filename",
+                  help="write report to FILE", metavar="FILE")
+parser.add_option("-q", "--quiet",
+                  action="store_false", dest="verbose", default=True,
+                  help="don't print status messages to stdout")
+
+(options, args) = parser.parse_args()

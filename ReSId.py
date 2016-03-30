cc698d1539687f9161a6f784db70419676d712e3
AstroRobin <RobinCook9012@hotmail.com>
2016-03-30 10:17:56 +0800

    Added to_Aegean_table() function
    to_Aegean_table() takes in the source data that have been constrained by the set RA and DEC and outputs a table in a format that Aegean can understand.

--- ReSId.py
+++ ReSId.py
@@ -53,9 +53,11 @@ def extract_sources(in_data, ref, RA, DEC, ang_diam, freq):
 	Find sources that are positioned with the dimensions of the image specified.
 	
 	:param in_data: data of all sources
+	:param ref: reference dictionary for column names
 	:param RA: right ascension of the image
 	:param DEC: declination of the image
 	:param ang_diam: angular diameter of the image
+	:param freq: central frequency of the map being used. (not implemented)
 	"""
 	sources_data = []
 	print RA, DEC
@@ -68,6 +70,59 @@ def extract_sources(in_data, ref, RA, DEC, ang_diam, freq):
 	print len(sources_data)
 	
 	return sources_data
+	
+def to_Aegean_table(source_data, ref, c_freq, base_name):
+	"""
+	configures source data into a format that Aegean/AeRes can understand.
+	
+	:param source_data: data arrays for sources constrained by RA and DEC values.
+	:param ref: reference dictionary for column names
+	:param c_freq: central frequency of the data
+	"param base_name: The base name for the output Aegean formatted table
+	"""
+	
+	out_file = open(base_name+'.csv','w')
+	out_file.write('island,source,background,local_rms,ra_str,dec_str,ra,err_ra,dec,err_dec,peak_flux,err_peak_flux,int_flux,err_int_flux,a,err_a,b,err_b,pa,err_pa,flags,residual_mean,residual_std,uuid,psf_a,psf_b,psf_pa\n')
+		
+	num_sources = len(source_data)
+	kk = 0
+	for ii in range(0,num_sources):
+		out_file.write(str(kk)+',') #island: 0 -> num_sources
+		kk += 1
+		out_file.write('0'+',') #source = 0
+		for jj in range(ref['background_'+c_freq],ref['local_rms_'+c_freq]): # columns encompassing background/rms information
+			out_file.write(str(source_data[ii][jj])+',')
+		
+		for jj in range(ref['ra_str'],ref['err_DECJ2000']): # columns encompassing RA and DEC information
+			out_file.write(str(source_data[ii][jj])+',')
+		
+		# Take int_flux <=> peak_flux
+		out_file.write(str(source_data[ii][ref['int_flux_'+c_freq]])+','+str(source_data[ii][ref['err_fit_flux_'+c_freq]])+',') #peak_flux + err
+		out_file.write(str(source_data[ii][ref['int_flux_'+c_freq]])+','+str(source_data[ii][ref['err_fit_flux_'+c_freq]])+',') #int_flux + err
+	
+		# write Gaussian parameters
+		out_file.write(str(source_data[ii][ref['a_'+c_freq]])+','+'0.00'+',') # a + err
+		out_file.write(str(source_data[ii][ref['b_'+c_freq]])+','+'0.00'+',') # b + err
+		out_file.write(str(source_data[ii][ref['pa_'+c_freq]])+','+'0.00'+',') # pa + err
+		
+		for jj in range(ref['residual_mean_'+c_freq],ref['residual_std_'+c_freq]): # residual mean and residual std columns
+			out_file.write(str(source_data[ii][jj])+',')
+		
+		# uuid
+		out_file.write('None'+',')
+		
+		# psf a/b and pa
+		for jj in range(ref['psf_a_'+c_freq],ref['psf_pa_'+c_freq]): # residual mean and residual std columns
+			if jj == ref['psf_pa_'+c_freq]-1:  # last column -> insert newline char
+				out_file.write(str(source_data[ii][jj])+'\n')
+			else:
+				out_file.write(str(source_data[ii][jj])+',')
+	
+	out_file.close()
+	
+	
+	
+	
 	
 def main():
 	usage = "usage: %prog [options] filename.fits"
@@ -93,6 +148,9 @@ def main():
 	parser.add_option("-a","--angulardiameter",
 					  action="store", type="float", dest="ang_diameter",default=2.0,
 					  help="angular diameter of the sides of the image",metavar="ANGULAR_DIAMETER")
+	parser.add_option("-b","--base",
+					  action="store", type="string", dest="base_name",default="ReSId_out",
+					  help="The base name for the output Aegean formatted table")
 	
 	
 	#parser.add_option("-","--",
@@ -104,7 +162,8 @@ def main():
 	
 	(in_data, column_ref) = read_data(options.data_filename)
 	
-	sources_data = extract_sources(in_data,column_ref,options.ra_map,options.dec_map,options.ang_diameter,options.central_freq)
-
+	source_data = extract_sources(in_data,column_ref,options.ra_map,options.dec_map,options.ang_diameter,options.central_freq)
+	
+	to_Aegean_table(source_data,column_ref,options.central_freq,options.base_name)
 
 main()
